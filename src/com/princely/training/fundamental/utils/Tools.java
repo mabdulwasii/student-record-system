bead1748fcb0450e60a7a7ea8be8da1aaee75046
Abu Abdullah <mabdulwasii@gmail.com>
2023-03-25 15:46:52 +0100

    feat: implement list operation

--- src/com/princely/training/fundamental/utils/Tools.java
+++ src/com/princely/training/fundamental/utils/Tools.java
@@ -1,5 +1,17 @@
 package com.princely.training.fundamental.utils;
 
 public class Tools {
+  //method to show operation list.
+  public static void listOperations() {
 
+    System.out.printf(
+        "  1. Add new student %n" +
+            "  2. View All student list %n" +
+            "  3. view a student from the list %n" +
+            "  4. Delete a student from the list %n" +
+            "  5. Delete all student from the list %n" +
+            "  6. Exit the program %n");
+
+    System.out.println(" ");
+  }
 }
