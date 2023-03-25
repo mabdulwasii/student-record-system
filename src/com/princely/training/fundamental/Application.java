2af607f291121852e37f59afef75ba2eac6ef1e6
Abu Abdullah <mabdulwasii@gmail.com>
2023-03-25 15:51:29 +0100

    feat: create student switch case

--- src/com/princely/training/fundamental/Application.java
+++ src/com/princely/training/fundamental/Application.java
@@ -1,9 +1,30 @@
 package com.princely.training.fundamental;
 
+import com.princely.training.fundamental.utils.Tools;
+import java.util.Scanner;
+
 public class Application {
 
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
+    int operation;
+
+    do {
+      System.out.println("STUDENT REGISTRATION FORM PROGRAM WITH THE FOLLOWING OPTION");
+      Tools.listOperations();
+      System.out.print("Enter your prefer option : ");
+      operation = scanner.nextInt();
+
+      switch (operation) {
+        case 1:
+          //Create new student
+          Tools.createNewStudent();
+          System.out.println(" Student added successfully ");
+          System.out.println(" ");
+          break;
+      }
+    } while (true);
   }
 
 }
